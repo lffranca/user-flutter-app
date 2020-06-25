cfca9dd7b085344f1aca6521b5f094b9292c14f7
Lucas França <lucasfranca.dev@gmail.com>
2020-06-24 22:59:44 -0300

    change url api

--- lib/settings/url.setting.dart
+++ lib/settings/url.setting.dart
@@ -1,4 +1,4 @@
 class URLSetting {
-  static final String urlApi = "http://192.168.15.6:8000/api";
+  static final String urlApi = "https://suflex.upowl.com.br/api";
   static final String prefsAccess = "9xA5crSAADLhJzrHqN6M";
 }
